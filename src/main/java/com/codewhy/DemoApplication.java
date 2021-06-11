a2a8cb9f21f16ccf9f0f4e44231a75d6de02c319
yamengZhen <3072807640@qq.com>
2021-06-11 23:11:06 +0800

    commit

---
+++ src/main/java/com/codewhy/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.codewhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class DemoApplication  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("ヾ(◍°∇°◍)ﾉﾞ    bootdo启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
+                " ______                    _   ______            \n" +
+                "|_   _ \\                  / |_|_   _ `.          \n" +
+                "  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
+                "  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
+                " _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
+                "|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
+    }
+    @PostConstruct
+    void started() {
+        TimeZone timeZone = TimeZone.getTimeZone("Asia/Shanghai");
+        TimeZone.setDefault(timeZone);
+    }
+
+}
---
+++ src/main/java/com/codewhy/common/util/LogEnum.java
@@ -0,0 +1,22 @@
+package com.codewhy.common.util;
+
+public enum LogEnum {
+    BUSSINESS("bussiness"),PLATFORM("platform"),DB("db"),EXCEPTION("exception");
+
+
+    private String category;
+
+
+    LogEnum(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
